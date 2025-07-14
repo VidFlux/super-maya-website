f8c8261c5ca641b5d372802ac8c87c19512bb99f
UniqueNIHAL <nihalhu05@gmail.com>
2025-07-14 19:25:25 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="content">
+      <h1 class="main-title">SUPER MAYA</h1>
+      <p class="subtitle">Operating System for Digital Ads</p>
+      <p class="tagline">COMING SOON!</p>
+    </div>
+  </div>
+`
---
+++ vite.config.js
@@ -0,0 +1,3 @@
+export default {
+  base: '/super-maya-website/',
+}; 
