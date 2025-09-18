68fd00c3008a8fbcd544ed90fe4dd28b6e5be050
Vitthal Gupta <31046067+VitthalGupta@users.noreply.github.com>
2025-09-18 23:57:56 +0530

    Update Calendly link for early access

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ document.querySelector('#app').innerHTML = `
     <div class="content">
       <h1 class="main-title">SUPERMAYA</h1>
       <p class="subtitle">An Operating System for Marketers</p>
-      <a href="https://calendly.com/sudhansu-thenirmata/30min" class="waitlist-btn" target="_blank" rel="noopener">GET EARLY ACCESS</a>
+      <a href="https://calendly.com/sudhansu-supermaya/30min" class="waitlist-btn" target="_blank" rel="noopener">GET EARLY ACCESS</a>
     </div>
   </div>
 `
